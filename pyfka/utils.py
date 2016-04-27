3f982ae50c49598bccb0a051834f705bc11dc0c9
Jakub Racek <jakub.racek@icloud.com>
2016-04-27 13:31:42 +0100

    Added utils

---
+++ pyfka/utils.py
@@ -0,0 +1,18 @@
+def json_encode(msg):
+    return str.encode(json.dumps(msg))
+
+def json_decode(msg):
+    return json.loads(msg.decode())
+
+
+def str_decode(msg):
+    if msg is None:
+        return None
+
+    return msg.decode()
+
+def str_encode(msg):
+    if msg is None:
+        return None
+
+    return str.encode(msg)
